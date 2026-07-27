e72b2234ab7c044ed9b745f465ea37e3d4df55c3
FinTrack Developer <developer@fintrackpro.com>
2026-07-28 02:03:43 +0530

    Fix Vercel build memory crash by restricting worker threads and webpack parallelism

--- next.config.js
+++ next.config.js
@@ -8,6 +8,14 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    cpus: 1,
+    workerThreads: false,
+  },
+  webpack: (config) => {
+    config.parallelism = 1;
+    return config;
+  },
 };
 
 module.exports = nextConfig;
